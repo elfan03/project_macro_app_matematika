388f2601b57fcc189680207d6bd3f3964aa1ceb5
Agum Pratama <pratamaagum15@gmail.com>
2023-10-16 23:22:55 +0700

    Fungsi menu Quiz

--- app/src/main/java/com/example/matematika_fun_project_macro/Home.kt
+++ app/src/main/java/com/example/matematika_fun_project_macro/Home.kt
@@ -1,11 +1,26 @@
 package com.example.matematika_fun_project_macro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnQuiz: Button = findViewById(R.id.btn_quiz)
+        btnQuiz.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.btn_quiz ->{
+                val intent = Intent(this@Home, MenuQuiz::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
